7a7b98b541377fa0f1561ff8e7d8974a53be9191
ThomasKenyeres <kenyerest97@gmail.com>
2017-01-16 18:37:32 +0100

    codecool_class partly done(to implement: methods)

--- codecool_class.py
+++ codecool_class.py
@@ -3,4 +3,13 @@ from student import Student
 
 
 class CodecoolClass:
-    pass
+    def __init__(self, location, year, mentors, students):
+        self.location = ""
+        self.year = 0
+        self.mentors = []
+        self.students = []
+
+    def find_student_by_full_name(self, full_name):
+        # TODO: implement thi$
+
+    
