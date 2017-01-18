5e08296e2586cd52db916fcdd7a6a1ab241af4ec
László Székely-Tóth <notisracer@gmail.com>
2017-01-18 12:06:44 +0100

    codecoolclass __init__ and generate_local done

--- codecool_class.py
+++ codecool_class.py
@@ -4,10 +4,17 @@ from student import Student
 
 class CodecoolClass:
     def __init__(self, location, year, mentors, students):
-        self.location = ""
-        self.year = 0
-        self.mentors = []
-        self.students = []
+        self.location = str(location)
+        self.year = int(year)
+        self.mentors = list(mentors)
+        self.students = list(students)
 
     def find_student_by_full_name(self, full_name):
         # TODO: implement thi$
+        pass
+
+    @classmethod
+    def generate_local(cls):
+        local_mentors = []
+        local_students = []
+        return cls("msc", 2016, local_mentors, local_students)
