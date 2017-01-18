48db687291f0689b43ff941b6ac69e5aacbd8f41
David Szilagyi <szilagyidav@gmail.com>
2017-01-18 11:52:21 +0100

    Mentor class is done

--- mentor.py
+++ mentor.py
@@ -2,11 +2,20 @@ from person import Person
 
 
 class Mentor(Person):
-    def __init__(self, first_name, last_name, year_of_birth, gender, nickname, device):
+    def __init__(self, first_name, last_name, year_of_birth, gender, nickname):
         super().__init__(first_name, last_name, year_of_birth, gender)
         self.nickname = nickname
-        self.device = Device()
+        self.device = None
 
-    @property
-    def fullname(self):
-        return self.first_name + " " self.last_name
+    def get_device(self, name, price, is_broken=False):
+        self.device = Device(name, price, is_broken)
+
+    @classmethod
+    def create_by_csv(cls, file_name):
+        with open(file_name, "r") as csv_file:
+            lines = csv_file.readlines()
+        mentors_list = []
+        for line in lines:
+            first, last, birth, gender, nickname = line.split(",")
+            mentors_list.append(cls(first, last, birth, gender, nickname))
+        return mentors_list
