61c56838b034e02fd632cf0eabb718d0e8611f0e
László Székely-Tóth <notisracer@gmail.com>
2017-01-18 22:15:18 +0100

    fixed ex_mentor alcohol level property not defined

--- ex_mentor.py
+++ ex_mentor.py
@@ -4,6 +4,7 @@ from mentor import Mentor
 class Ex_Mentor(Mentor):
     def __init__(self, first_name, last_name, year_of_birth, gender, nickname, device):
         super().__init__(first_name, last_name, year_of_birth, gender, nickname, device)
+        self.alcohol_level = 0
 
     def set_alcohol_level(self, amount):
         self.alcohol_level += amount
