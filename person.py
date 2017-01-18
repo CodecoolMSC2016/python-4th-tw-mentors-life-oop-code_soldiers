788d719877977f5941f94725ed1a88fc71fc8f96
David Szilagyi <szilagyidav@gmail.com>
2017-01-18 11:53:26 +0100

    Person get a full name property

--- person.py
+++ person.py
@@ -4,3 +4,7 @@ class Person:
         self.last_name = str(last_name)
         self.year_of_birth = int(year_of_birth)
         self.gender = str(gender)
+
+    @property
+    def fullname(self):
+        return self.first_name + " " self.last_name
