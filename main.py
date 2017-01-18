f652e00b49db323414293ab0c3aa8b103dd21fe8
David Szilagyi <szilagyidav@gmail.com>
2017-01-18 22:36:56 +0100

    Main module implemented

---
+++ main.py
@@ -0,0 +1,22 @@
+from os import system
+from time import sleep
+
+system("python3 story.py")
+sleep(2)
+print("Ooooppppps 😣....\nThere was an error during running 'story.py' 😣 .")
+sleep(5)
+for count in range(3):
+    for index in range(4):
+        text = "Starting auto debugger module" + "." * index
+        print(text, end='\r\r')
+        sleep(0.5)
+system("clear")
+system("python3 debugger.py")
+for count in range(3):
+    system("clear")
+    for index in range(4):
+        text = "Starting 'story.py'" + "." * index
+        print(text, end='\r\r')
+        sleep(0.5)
+system("clear")
+system("python3 story.py")
