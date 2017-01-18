e1262dd3778516758dde66a2b09026ffdc41d106
ThomasKenyeres <kenyerest97@gmail.com>
2017-01-18 14:41:18 +0100

    boss.py completed

---
+++ boss.py
@@ -0,0 +1,14 @@
+from person import Person
+
+class Boss(Person):
+    def __init__(self, first_name, last_name, year_of_birth, gender, nickname, salary, anger_level):
+        super().__init__(first_name, last_name, year_of_birth, gender)
+        self.nickname = str(nickname)
+        self.salary = int(salary)
+        self.anger_level = int(anger_level)
+
+    def get_salary(self):
+        return "__topsecret__"
+
+    def change_anger_level(self, value):
+        self.anger_level += value
