9ad3af656fbea66f69dc4c66ece8a9e348ab592b
David Szilagyi <szilagyidav@gmail.com>
2017-01-18 12:05:07 +0100

    Beverage.py moved to correct direction

--- specifications/beverage.py
+++
@@ -1,9 +0,0 @@
-class Beverage:
-    drink_types = ["vodka", "coffee", "coke"]
-
-    def __init__(self, name, drink_type):
-        self.name = name
-        if drink_type in drink_types:
-            self.drink_type = drink_type
-        else:
-            self.drink_type = "vodka"
