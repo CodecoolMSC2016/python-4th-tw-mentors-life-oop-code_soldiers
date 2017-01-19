a5509d05363201d3a586488dd33d1a1411aac477
László Székely-Tóth <notisracer@gmail.com>
2017-01-19 11:22:20 +0100

    story bugfixes

--- story.py
+++ story.py
@@ -6,10 +6,10 @@ import time
 
 print("\n"*20)
 text = ["A long time ago in a galaxy far, far away...",
-        "An man from Bangladur, Humen Rajtnau was tired of mentorbots,"
-        "so he got started to implement some new extra features,"
-        "but he did not realise a bug,"
-        "which not only could destroy the entire CodeCool,"
+        "An man from Bangladur, Humen Rajtnau was tired of mentorbots,",
+        "so he got started to implement some new extra features,",
+        "but he did not realise a bug,",
+        "which not only could destroy the entire CodeCool,",
         "but the whole programmer community..."
         ]
 
@@ -19,12 +19,12 @@ error_message = 'Traceback (most recent call last):\n  File "<stdin>", line 36, 
 
 def print_text_slowly(lines, sleep):
     max_len = str(max(lines, key=len))
-    
+
     for line in lines:
-        print line
+        print(line)
         time.sleep(sleep)
 
-print_text_slowly(text, 0.5)
+print_text_slowly(text, 1)
 
 
 
@@ -38,9 +38,9 @@ print("                     |  `----.|  `--'  | |  '--'  ||  |____              
 print("                      \______| \______/  |_______/ |_______|                      ")
 print("     _______.  ______    __       _______   __   _______ .______          _______.")
 print("    /       | /  __  \  |  |     |       \ |  | |   ____||   _  \        /       |")
-print("   |   (----`|  |  |  | |  |     |  .--.  ||  | |  |__   |  |_)  |      |   (----`")
 print(error_message)
 sys.exit(0)
+print("   |   (----`|  |  |  | |  |     |  .--.  ||  | |  |__   |  |_)  |      |   (----`")
 print("    \   \    |  |  |  | |  |     |  |  |  ||  | |   __|  |      /        \   \    ")
 print(".----)   |   |  `--'  | |  `----.|  '--'  ||  | |  |____ |  |\  \----.----)   |   ")
 print("|_______/     \______/  |_______||_______/ |__| |_______|| _| `._____|_______/    ")
