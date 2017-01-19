03c03d1d89f9d90bf2c2b1bd4a4ba7f5d7f2e244
László Székely-Tóth <notisracer@gmail.com>
2017-01-19 13:04:26 +0100

    story description and improvements

--- story.py
+++ story.py
@@ -5,17 +5,21 @@ import sys
 import time
 import os
 
+
+os.system("clear")
 print("A long time ago in a galaxy far, far away...")
 time.sleep(3)
 os.system("clear")
-print("\n"*50)
+print("\n" * 30)
 text = ["A man from India, Humen Rajtnau was tired of mentorbots,",
-        "so he got started to implement some new extra features,",
-        "but he did not realise a bug,",
-        "which not only could destroy the entire CodeCool,",
-        "but the whole programmer community..."
-        ]
+        "so he got started to implement  some new extra features,",
+        "but he became  so lazy  he gave his job  to codecoolers.",
+        "",
+        "Unfortunately   they  did  not  realise  a  deadly  bug,",
+        "which  not  only  could  destroy  the  entire  CodeCool,",
+        "but the whole programmer community..."]
 
+text += ["" for x in range(30)]
 
 def print_text_slowly(lines, sleep):
     max_len = str(max(lines, key=len))
@@ -24,7 +28,9 @@ def print_text_slowly(lines, sleep):
         print(line)
         time.sleep(sleep)
 
-print_text_slowly(text, 1.33)
+
+print_text_slowly(text, 0.6)
+os.system("clear")
 
 error_message = 'Traceback (most recent call last):\n  File "<stdin>", line 36, in <module>\n' \
                 "IndentationError: expected an indented block"
@@ -38,12 +44,24 @@ print("                     |  `----.|  `--'  | |  '--'  ||  |____              
 print("                      \______| \______/  |_______/ |_______|                      ")
 print("     _______.  ______    __       _______   __   _______ .______          _______.")
 print("    /       | /  __  \  |  |     |       \ |  | |   ____||   _  \        /       |")
-print(error_message)
-sys.exit(0)
+# print(error_message)
+# sys.exit(0)
 print("   |   (----`|  |  |  | |  |     |  .--.  ||  | |  |__   |  |_)  |      |   (----`")
 print("    \   \    |  |  |  | |  |     |  |  |  ||  | |   __|  |      /        \   \    ")
 print(".----)   |   |  `--'  | |  `----.|  '--'  ||  | |  |____ |  |\  \----.----)   |   ")
 print("|_______/     \______/  |_______||_______/ |__| |_______|| _| `._____|_______/    ")
 print("\n\n                          A Mentor's Life (in (n)OO way)")
+time.sleep(5)
 
 codecool_msc = CodecoolClass.generate_local()
+print("<< CODECOOL_MSC GENERATED >>")
+print("One day when Humen Rajtnau went to Codecool Miskolc")
+
+""" mindenki exmentor lesz, együtt elkezdenek inni,
+annyira részegek lesznek, hogy elpusztítják a codecoolt és mindenkit aki tud programozni,
+így pusztítják el a programozói közösséget.
+kiprinteljen egy olyat, hogy a codecool bp is destroyed, codecool msc destroyed stb
+a sírástól zárlatosak lesznek, és vészüzemmódba teszik magukat,
+amíg a megfelelő emberek újra nem programozák őket
+
+"""
