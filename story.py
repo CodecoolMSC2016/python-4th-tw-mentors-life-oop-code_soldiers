6607c4ec49b4b8fb095dacca097470ffee379ef1
ThomasKenyeres <kenyerest97@gmail.com>
2017-01-19 10:46:53 +0100

    story started

--- story.py
+++ story.py
@@ -2,6 +2,27 @@ from codecool_class import CodecoolClass
 from mentor import Mentor
 from student import Student
 import sys
+import time
+
+print("\n"*20)
+text = ["A long time ago in a galaxy far, far away...",
+        ""
+        ]
+
+error_message = 'Traceback (most recent call last):\n  File "<stdin>", line 1, in <module>\n' \
+                "FileNotFoundError: [Errno 2] No such file or directory: 'codecool_class.py'"
+
+
+def print_text_slowly(lines, sleep):
+    for line in text:
+        print line
+        time.sleep(sleep)
+
+print_text_slowly(text, 40)
+
+
+
+
 
 print("                       ______   ______    _______   _______                       ")
 print("                      /      | /  __  \  |       \ |   ____|                      ")
@@ -12,15 +33,11 @@ print("                      \______| \______/  |_______/ |_______|             
 print("     _______.  ______    __       _______   __   _______ .______          _______.")
 print("    /       | /  __  \  |  |     |       \ |  | |   ____||   _  \        /       |")
 print("   |   (----`|  |  |  | |  |     |  .--.  ||  | |  |__   |  |_)  |      |   (----`")
+print(error_message)
+sys.exit(0)
 print("    \   \    |  |  |  | |  |     |  |  |  ||  | |   __|  |      /        \   \    ")
 print(".----)   |   |  `--'  | |  `----.|  '--'  ||  | |  |____ |  |\  \----.----)   |   ")
 print("|_______/     \______/  |_______||_______/ |__| |_______|| _| `._____|_______/    ")
 print("\n\n                          A Mentor's Life (in (n)OO way)")
 
-error_message = 'Traceback (most recent call last):\n  File "<stdin>", line 1, in <module>\n' \
-                "FileNotFoundError: [Errno 2] No such file or directory: 'codecool_class.py'"
-
-#print(error_message)
-#sys.exit(0)
 codecool_msc = CodecoolClass.generate_local()
-print(codecool_msc)
